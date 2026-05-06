c1fa8e9c2dd72c08694ca8224d4b1fa1289642d5
Willian Martins Silva <00wms00@gmail.com>
2026-05-06 11:10:12 -0400

    Add simple VehiclePolicy for update/delete authorization

---
+++ app/Policies/VehiclePolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Vehicle;
+
+class VehiclePolicy
+{
+    public function update(User $user, Vehicle $vehicle): bool
+    {
+        return $vehicle->user_id === $user->id;
+    }
+
+    public function delete(User $user, Vehicle $vehicle): bool
+    {
+        return $vehicle->user_id === $user->id;
+    }
+}
